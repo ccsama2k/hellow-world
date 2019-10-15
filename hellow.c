28af253e7eec45f20eecbf15f0da82646e3d7540
ccsama2k <55731355+ccsama2k@users.noreply.github.com>
2019-10-15 14:15:00 +0800

    Create hellow.c

---
+++ hellow.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+#include <curl/curl.h> 
+int main(void) { 
+CURL *cunl;
+CURLcode res; 
+curl=curl_easy_init();
+if(curl){ 
+ curl_easy_setopt(curl,CURLOPT_URL,"http://myip.ipip.net/");
+ res=curl_easy_strerror(res));
+ curl_easy_cleanup(curl);
+ }
+return0;
+}
