41b167f7757536d7148d22523c773dd5527dc48b
ccsama2k <55731355+ccsama2k@users.noreply.github.com>
2019-11-25 20:42:06 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,13 +1,17 @@
-#include <stdio.h> 
-#include <curl/curl.h> 
-int main(void) { 
-CURL *cunl;
-CURLcode res; 
+#include<stdio.h>
+#include<curl/curl.h>
+
+int main()
+{
+CURL *curl;
+CURLcode res;
 curl=curl_easy_init();
-if(curl){ 
- curl_easy_setopt(curl,CURLOPT_URL,"http://myip.ipip.net/");
- res=curl_easy_strerror(res));
- curl_easy_cleanup(curl);
+if(curl){
+curl_easy_setopt(curl,CURLOPT_URL,"http:/myip.ipip.net/");
+res=curl_easy_perform(curl);
+if(res!=CURLE_OK)
+  fprintf(stderr,"curl_easy_perform() failed:%s\n",curl_easy_strerror(res));
+  curl_easy_cleanup(curl);
  }
-return 0;
+ return 0;
 }
